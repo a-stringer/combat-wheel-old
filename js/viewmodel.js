91c615c1ffe57c69cbdcaa8aa9e05b2c948c63ba
Alec Stringer <alec.stringer@go2advertising.com>
2016-09-09 17:37:39 -0400

    Finished Player Character Roster

--- js/viewmodel.js
+++ js/viewmodel.js
@@ -1,20 +1,30 @@
-function playerCharacter(name, dvMod, joinBattle){
+function partyMember(name, dvMod, init){
   var self = this;
 
   self.name = name;
+  self.initials = ko.computed(function(){return self.name.charAt(0).toUpperCase();});
   self.dvMod = ko.observable(dvMod);
-  self.joinBattle = ko.observable(joinBattle);
+  self.init = ko.observable(init);
 }
 
-function AppViewModel() {
+function CombatWheelViewModel() {
+  var self = this;
+
   self.party = ko.observableArray([
-    new playerCharacter("Lexi", 0, 0),
-    new playerCharacter("Ian", 0, 0),
-    new playerCharacter("Ed", 0, 0),
-    new playerCharacter("Mark", 0, 0),
-    new playerCharacter("Ryan", 0, 0)
+    new partyMember("Lexi", 0, 0),
+    new partyMember("Ian", 0, 0),
+    new partyMember("Ed", 0, 0),
+    new partyMember("Mark", 0, 0),
+    new partyMember("Ryan", 0, 0)
   ]);
 
+  self.addPartyMember = function(){
+    self.party.push(new partyMember("unnamed", 0, 0));
+  };
+
+  self.removePartyMember = function(member){
+    self.party.remove(member)
+  };
 }
 
-ko.applyBindings(new AppViewModel());
+ko.applyBindings(new CombatWheelViewModel());
