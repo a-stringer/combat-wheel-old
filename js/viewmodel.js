7994e8a15973658a499d8b18359f2defcbc26581
Alec Stringer <alec.stringer@go2advertising.com>
2016-09-12 16:51:57 -0400

    party members can be added to inBattle array

--- js/viewmodel.js
+++ js/viewmodel.js
@@ -1,30 +1,39 @@
-function partyMember(name, dvMod, init){
+function PartyMember(name, dvMod, init){
   var self = this;
 
   self.name = name;
-  self.initials = ko.computed(function(){return self.name.charAt(0).toUpperCase();});
   self.dvMod = ko.observable(dvMod);
   self.init = ko.observable(init);
-}
+};
+
 
 function CombatWheelViewModel() {
   var self = this;
 
   self.party = ko.observableArray([
-    new partyMember("Lexi", 0, 0),
-    new partyMember("Ian", 0, 0),
-    new partyMember("Ed", 0, 0),
-    new partyMember("Mark", 0, 0),
-    new partyMember("Ryan", 0, 0)
+    new PartyMember("Lexi", 0, 0),
+    new PartyMember("Ian", 0, 0),
+    new PartyMember("Ed", 0, 0),
+    new PartyMember("Mark", 0, 0),
+    new PartyMember("Ryan", 0, 0)
   ]);
 
+  self.inBattle = ko.observableArray();
+
+
   self.addPartyMember = function(){
-    self.party.push(new partyMember("unnamed", 0, 0));
+    self.party.push(new PartyMember("unnamed", 0, 0));
   };
 
   self.removePartyMember = function(member){
     self.party.remove(member)
   };
+
+  self.joinBattle = function(member){
+    var index = self.party.indexOf(member);
+    self.inBattle.push(self.party.slice(index, (index + 1)));
+    //console.log(self.inBattle()[0][0].init());
+  }
 }
 
 ko.applyBindings(new CombatWheelViewModel());
