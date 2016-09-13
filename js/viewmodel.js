02a36dbdbda0b5937d08e353729d4a9481ee5164
Alec Stringer <alec.stringer@go2advertising.com>
2016-09-13 12:39:53 -0400

    Initial Battle Order now loads; pushing live.

--- js/viewmodel.js
+++ js/viewmodel.js
@@ -6,9 +6,17 @@ function PartyMember(name, dvMod, init){
   self.init = ko.observable(init);
 };
 
+function BattleTick(tickNo, actsOnThisTick){
+  var self = this;
+
+  self.tickNo = tickNo;
+  self.actsOnThisTick = ko.observable(actsOnThisTick);
+};
 
 function CombatWheelViewModel() {
   var self = this;
+
+  self.battleRunning = ko.observable(false);
 
   self.party = ko.observableArray([
     new PartyMember("Lexi", 0, 1),
@@ -18,8 +26,20 @@ function CombatWheelViewModel() {
     new PartyMember("Ryan", 0, 5)
   ]);
 
+  self.battleOrder = ko.observableArray([
+    new BattleTick(0,''),
+    new BattleTick(1,''),
+    new BattleTick(2,''),
+    new BattleTick(3,''),
+    new BattleTick(4,''),
+    new BattleTick(5,''),
+    new BattleTick(6,''),
+    new BattleTick(7,'')
+  ]);
+
   self.inBattle = ko.observableArray();
 
+  // Operations
   self.addPartyMember = function(){
     self.party.push(new PartyMember("unnamed", 0, 0));
   };
@@ -31,8 +51,19 @@ function CombatWheelViewModel() {
   self.joinBattle = function(member){
     var index = self.party.indexOf(member);
     self.inBattle.push(self.party.slice(index, (index + 1)));
-  }
+  };
+
   self.startBattle = function(){
+    self.battleRunning(true);
+    self.convertInits();
+    self.placeCombatants();
+  };
+
+  self.endBattle = function(){
+    self.battleRunning(false);
+  };
+
+  self.convertInits = function(){
     var highestInit = 0;
     for (i = 0; i < self.inBattle().length; i++) {
       highestInit = (highestInit < self.inBattle()[i][0].init()) ? self.inBattle()[i][0].init() : highestInit;
@@ -42,6 +73,14 @@ function CombatWheelViewModel() {
     }
   };
 
+  self.placeCombatants = function(){
+    for (i = 0; i < self.inBattle().length; i++) {
+      console.log(self.inBattle()[i]);
+      var newInit = self.inBattle()[i][0].init();
+      var combatant = self.inBattle()[i][0].name;
+      self.battleOrder()[newInit].actsOnThisTick((self.battleOrder()[newInit].actsOnThisTick()) + combatant + ' ');
+    }
+  };
 }
 
 ko.applyBindings(new CombatWheelViewModel());
